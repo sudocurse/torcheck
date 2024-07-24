a1063205d92256c1f35bbff24a3481479f87a168
sudocurse <sudocurse@gmail.com>
2024-07-24 14:19:04 -0700

    /list endpoint

--- torcheck/__init__.py
+++ torcheck/__init__.py
@@ -1,14 +1,30 @@
 from flask import Flask
 from . import scheduler
+import json
+
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile("config.py")
+    cfg = app.config["SERVICES"]["tor"]
+
+    def define_response(data):
+        return app.response_class(
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
 
     @app.route('/source')
     def source():
-        return app.config["SERVICES"]["tor"]["node_list"]
+        data = {"source": cfg["node_list"]}
+        return define_response(data)
+
+    @app.route('/list')
+    def list():
+        data = {"nodes": cfg["nodes"]}
+        return define_response(data)
 
     scheduler.start_scheduler(app)
 
--- torcheck/scheduler.py
+++ torcheck/scheduler.py
@@ -10,20 +10,31 @@ def app_context():
     with app.app_context():
         yield
 
-def schedule_download(node_list, path):
+def schedule_download(node_list, path, cfg):
     try:
         response = requests.get(node_list)
         with open(path, "wb") as f:
             f.write(response.content)
+
+        # parse response.content to list and set config["SERVICES"]["tor"]["nodes"]
+        with app_context():
+            nodes = response.content.decode("utf-8").split("\r\n")
+            app.config["SERVICES"]["tor"]["nodes"] = [n.strip() for n in nodes]
+
     except Exception as e:
         raise Exception("Error downloading node list")
 
 def start_scheduler(app):
-    with app.app_context():
-        node_list = app.config["SERVICES"]["tor"]["node_list"]
-        path = os.path.join(app.instance_path, "tor_nodes.txt")
 
-    schedule_download(node_list, path)
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(schedule_download, "interval", seconds=60 * 60, args=[node_list, path])
-    scheduler.start()
+    with app.app_context():
+        cfg = app.config["SERVICES"]["tor"]
+        node_list = cfg["node_list"]
+        path = os.path.join(app.instance_path, "tor_nodes.txt")
+        interval = cfg["refresh_interval"]
+
+        schedule_download(node_list, path, app.config) # initial
+
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(schedule_download, "interval", seconds=interval,
+                          args=[node_list, path, cfg])
+        scheduler.start()
