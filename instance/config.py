e9ca3102dc4ea52a70fd19034082eaf5008996dd
sudocurse <sudocurse@gmail.com>
2024-07-24 21:11:55 -0700

    adding instance for demo purposes

---
+++ instance/config.py
@@ -0,0 +1,7 @@
+SERVICES = {
+    "tor": {
+        "source_url": "https://secureupdates.checkpoint.com/IP-list/TOR.txt",
+        "port": 443,
+        "refresh_interval": 60 * 60, # 1 hour
+    }
+}
